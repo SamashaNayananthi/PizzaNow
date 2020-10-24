d6104294524300300ef16acdcbb1ea6c2ca15749
SamashaNayananthi <samasha521@gmail.com>
2020-10-24 10:48:54 +0530

    about us done

--- application/views/about_us.php
+++ application/views/about_us.php
@@ -1,4 +1,4 @@
-<html>
+<html lang="en">
 
 <head>
 	<title>PizzaNow!</title>
@@ -11,8 +11,23 @@
 include_once("header.php");
 ?>
 
-<div id="title">
-	<h1>About PizzaNow</h1>
+<div id="desc">
+
+	<div id="title">About PizzaNow!</div>
+
+	<div id="float-child">
+		<div class="left-first">- Excellent Service <br>- Reasonable Prices <br>- Quick Delivery</div>
+	</div>
+
+	<div id="float-child">
+		<div class="right-first">We have 22 years of experience with Pizza... <br>
+			Long Story Short...<br>
+			PizzaNow! is inspired by a girl named Sam who love foods very much. Since childhood,
+			her future goal is to build a Pizza ordering website on her own. So, PizzaNow! is a live
+			proof of <b>dreams do come true!!!</b>
+		</div>
+	</div>
+
 </div>
 
 
