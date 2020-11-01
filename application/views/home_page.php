f027bb7db2280cb88f6f5b067594b7eda21ce8e8
SamashaNayananthi <samasha521@gmail.com>
2020-11-01 18:49:39 +0530

    fav icon added

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -4,6 +4,8 @@
 	<title>PizzaNow!</title>
 
 	<link href="/PizzaNow/css/homepage.css" rel="stylesheet">
+
+	<link rel="shortcut icon" type="image/x-icon" href="/PizzaNow/images/favicon.png" />
 </head>
 
 <body>
@@ -14,7 +16,7 @@ include_once("header.php");
 <div id="welcome">
 	<h1 id="h1">WELCOME !!! HUNGRY NOW !!! PIZZA NOW !!!</h1>
 	<p id="para">THERE’S NO BETTER FEELING IN THE WORLD THAN A WARM PIZZA BOX IN YOUR LAP<br>
-	SO WHAT ARE YOU WAITING FOR ??? <br> HURRY UP !!! AND GRAB YOU'RE PIZZA !!!</p>
+	SO WHAT ARE YOU WAITING FOR ??? <br> HURRY UP !!! AND GRAB YOUR PIZZA !!!</p>
 </div>
 
 <?php
